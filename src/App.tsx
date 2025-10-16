bd454b8fbf0aeb19840e259a2590e22a0a75ae12
oded <odedbabayov@gmail.com>
2025-10-16 12:05:55 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-50">Hello Tailwind v3 + Vite + React + TS!</div>
+    </>
+  );
+}
+
+export default App;
